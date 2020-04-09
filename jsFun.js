62080172cb1c2144d17d255e8de41fc2edc5d705
Napoleon Vuong <napolo101@gmail.com>
2020-04-08 18:55:22 -0700

    first commit

---
+++ jsFun.js
@@ -0,0 +1,26 @@
+// typeof to get datatype from thing
+console.log(typeof "Hello World");
+
+// not very sepcific
+console.log(typeof 5.5);
+
+var name = "Thomas";
+//Uppercases everything within the "name" String
+console.log(name.toUpperCase());
+//Lowercases everty letter within the string
+console.log(name.toLowerCase());
+
+
+var arr1 = ["Thomas", "William", "Smith"];
+// .length gives the length of the array
+console.log(arr1.length);
+
+//These lines of codes log the first and second items within the array arr1
+console.log(arr1[0]);
+console.log(arr1[1]);
+
+console.log(name[0]);
+// this code^ & the code below do the same thing
+console.log(name.charAt(0));
+
+// Open up the terminal and type: " node jsFun.js " to see the console logs
